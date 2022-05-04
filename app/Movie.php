51456433da12f310708e025233a5228c9c180665
giacomo1235 <giacomo.passariello@gmail.com>
2022-05-04 17:40:10 +0200

    layout

--- app/Movie.php
+++ app/Movie.php
@@ -7,5 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     // public $timestamps = false;
-    //public $table = 'laravel_model_controller';
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,17 @@
 @section('title', 'Books Listing')
 
 @section('main')
-    <ul>
+    <div class="container">
         @foreach ($movies as $movie)
-            <li>{{ $movie->title }}</li>
+            
+            <div class="card">
+                <img src="{{$movie->locandina}}" alt="">
+                <p><b>Titolo:</b>{{ $movie->title }}</p>
+                <p><b>Titolo originale:</b>{{ $movie->original_title }}</p>
+                <p><b>Voto:</b>{{ $movie->vote }}</p>
+                <p><b>Nazione:</b>{{ $movie->nationality }}</p>
+                
+            </div>
         @endforeach
-    </ul>
+    </div>
 @endsection
